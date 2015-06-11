cad580350f3963f1051050fca92b73b171d21862
Mindy Mallory <mindy.mallory@gmail.com>
2015-06-11 14:21:02 -0500

    basically working, need to add description and more details

--- server.R
+++ server.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(ggplot2)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -18,33 +19,42 @@ shinyServer(function(input, output) {
     t   = input$duration/251
     T   = input$daystomat/251
     vol = input$volatiltiy/100
+ 
+       # BS Price of option when sold
+    C <- function(x){pnorm((log(x/K)+(r+vol^2/2)*(T-t))/(vol*sqrt((T-t))))*x -
+        pnorm((log(x/K)+(r+vol^2/2)*(T-t))/(vol*sqrt((T-t))) - vol*sqrt((T-t)))*K*exp(-r*(T-t))}
     
-    # Price of option when purchased
-    d1_ = (log(K/K)+(r+vol^2/2)*T)/(vol*sqrt(T))
-    d2_ = d1_ - vol*sqrt(T)
-    C_ = pnorm(d1_)*K-pnorm(d2_)*K*exp(-r*T)   #Call Price at Strike
+    # BS Price of option when purchased
+    C_ <- function(x){pnorm((log(x/K)+(r+vol^2/2)*(T))/(vol*sqrt((T))))*x -
+        pnorm((log(x/K)+(r+vol^2/2)*(T))/(vol*sqrt((T))) - vol*sqrt((T)))*K*exp(-r*(T))} 
     
-    # Price of option when sold
-    d1 = (log(S/K)+(r+vol^2/2)*(T-t))/(vol*sqrt((T-t)))
-    d2 = d1 - vol*sqrt((T-t))
-    C  = pnorm(d1)*S-pnorm(d2)*K*exp(-r*(T-t))
-    
-    curve(pnorm((log(x/K)+(r+vol^2/2)*T)/(vol*sqrt(T)))*x-pnorm((log(x/K)+(r+vol^2/2)*T)/(vol*sqrt(T))- vol*sqrt(T))*K*exp(-r*T), 
-          from = .90*K, to=1.1*K, xlab = "Price of Underlying", ylab = "Option Price", main = "Option Price")
+  X <- c(K, S)
+  Y <- c(C_(K), C(S))
+  df <- data.frame(X,Y)
+  GL = C(S) - C_(K)
+  g = toString(round(GL,2))
+  GL_Percent = GL/C_(K)*100
+  g_percent = paste0(toString(round(GL_Percent,2)), '%')
+ 
+#    ggplot(df, aes(x=X, y=Y)) + 
+#                        geom_point() +
+#                        stat_function(fun = C) +
+#                        stat_function(fun = C_) +
+#                        xlim(0.95*K, 1.05*K) 
+#                        
+  #plot.window(c(.90*K,1.1*K), c(.9*C(S), 1.1*C(S)))
+  curve(pnorm((log(x/K)+(r+vol^2/2)*T)/(vol*sqrt(T)))*x-pnorm((log(x/K)+(r+vol^2/2)*T)/(vol*sqrt(T))- vol*sqrt(T))*K*exp(-r*T), 
+        from = .95*K, to=1.05*K, xlab = "Price of Underlying", ylim= c(0,3*C(S)), ylab = "Option Price", main = "Option Price", col="black")
     par(new=TRUE)
-    curve(pnorm((log(x/K)+(r+vol^2/2)*(T-t))/(vol*sqrt((T-t))))*x-pnorm((log(x/K)+(r+vol^2/2)*(T-t))/(vol*sqrt((T-t)))- vol*sqrt((T-t)))*K*exp(-r*(T-t)), 
-          from = .90*K, to=1.1*K, xlab = "Price of Underlying", ylab = "Option Price", main = "Option Price")
-   
-    GL = C - C_
-    g = toString(round(GL,2))
-    GL_Percent = GL/C_*100
-    g_percent = paste0(toString(round(GL_Percent,2)), '%')
+  curve(pnorm((log(x/K)+(r+vol^2/2)*(T-t))/(vol*sqrt((T-t))))*x-pnorm((log(x/K)+(r+vol^2/2)*(T-t))/(vol*sqrt((T-t)))- vol*sqrt((T-t)))*K*exp(-r*(T-t)), 
+        from = .95*K, to=1.05*K, xlab = "Price of Underlying", ylim= c(0,3*C(S)),ylab = "Option Price", main = "Option Price", col="red")
     par(new=TRUE)
-    plot(K,C_, xlim = .90*K, ylim=1.1*K, xlab = "Price of Underlying", ylab = "Option Price", main = "Option Price")
-    par(new=TRUE)
-    plot(S,C, xlim = .90*K, ylim=1.1*K, xlab = "Price of Underlying", ylab = "Option Price", main = "Option Price")
+#   plot(K,C_(K), xlim =c(.90*K,1.1*K), xlab = "Price of Underlying", ylab = "Option Price", main = "Option Price")
+#     par(new=TRUE)
+#   plot(S,C(S), xlim =c(.90*K,1.1*K), xlab = "Price of Underlying", ylab = "Option Price", main = "Option Price")
     legend("topright", inset = .05, title = "Option Gain/Loss", c(g, g_percent))
-    
+        abline(v=S)
+        abline(v=K)   
 #     #Graphing the Position Value
 #     curve(q*Q*(pnorm((log(x/K)+(r+vol^2/2)*t)/(vol*sqrt(t)))*x-pnorm((log(x/K)+(r+vol^2/2)*t)/(vol*sqrt(t))- vol*sqrt(t))*K*exp(-r*t)), 
 #           from = .9*K, to=1.1*K, xlab = "Price of Underlying", ylab = "Position Value", main = "Position Value")
@@ -55,8 +65,5 @@ shinyServer(function(input, output) {
 #     GL = PosV - PosV_
 #     g = toString(round(GL,2))
 #     legend("topright", inset = .05, title = "Gain/Loss", c(g))
-    
-    
-    
-  })
+   })
 })
